d3018430672a395377660a4a1eeb3ce43f4970ca
Daniel Pernia <danielp299@gmail.com>
2024-04-04 02:30:04 -0300

    ERC721Enumerable Paged

---
+++ ERC721EnumerableExtra.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.9/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+pragma solidity >=0.7.0 <0.9.0;
+
+abstract contract ERC721EnumerableExtra is ERC721Enumerable
+{
+    function tokenOfOwnerByPage(address owner,uint32 page) external view returns(uint256[20] memory items){
+            for(uint8 i = 0; i < 20 ; i++){
+            if(i + (20 * page) < balanceOf(owner)){
+                items[i] = tokenOfOwnerByIndex(owner,i + (20 * page));  
+            }else{
+                break;
+            }
+        }
+    }
+}
